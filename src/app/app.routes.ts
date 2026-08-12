d25e38a724d928cabbb4daf9a9e5d919c1de554f
Mourvan <mourvanhmaes@gmail.com>
2026-08-11 21:21:37 -0300

    teste-git

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'tasks',
+        component: TasksComponent
+    }
+];
