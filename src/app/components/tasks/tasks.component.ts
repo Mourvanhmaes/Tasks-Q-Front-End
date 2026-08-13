aae19726f228ece82d541574f8ee316dcdf6f748
Mourvan <mourvanhmaes@gmail.com>
2026-08-12 21:44:04 -0300

    feat: Criação do Front com cadastro e cards

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule} from 'mdb-angular-ui-kit/forms';
+
+
+enum Prioridade{
+  ALTA = 'ALTA',
+  MEDIA = 'MEDIA',
+  BAIXA = 'BAIXA'
+}
+
+interface Tasks{
+  titulo: string;
+  descricao: string;
+  prioridade: Prioridade;
+  prazo: string;
+  usuarioDestino: string;
+}
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [
+    FormsModule,
+    MdbFormsModule
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
 
+    Prioridade = Prioridade;
+    tasks: Tasks = {
+      titulo: '',
+      descricao: '',
+      prioridade: Prioridade.ALTA,
+      prazo: '',
+      usuarioDestino: ''
+    };
+
+    cadastrarTask(){
+      console.log(this.tasks)
+    }
 }
