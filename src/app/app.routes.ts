a02d8424c101e8a875f68a5fb6517d84dfd23543
Mourvan <mourvanhmaes@gmail.com>
2026-08-31 21:06:54 -0300

    feat: arquitetura inicial de pastas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,30 +1,7 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { UsersComponent } from './components/users/users.component';
-import { LoginComponent } from './components/login/login.component';
-import { PrincipalComponent } from './components/principal/principal.component';
 
 export const routes: Routes = [
   {
-    path: '',
-    component: LoginComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'admin',
-    component: PrincipalComponent,
-    children:[
-        {
-          path: 'tasks',
-          component: TasksComponent
-        },
-        {
-          path: 'users',
-          component: UsersComponent
-        },
-    ]
+
   }
 ];
