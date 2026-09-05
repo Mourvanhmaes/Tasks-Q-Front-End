5c4424ce4b83b4abfcd1d1c121842b6e2428ab0b
Mourvan Maes <mourvanhmaes@gmail.com>
2026-09-05 13:24:08 -0300

    feat: criação do visual e da model

---
+++ src/app/models/enums/status.enum.ts
@@ -0,0 +1,6 @@
+export enum Status {
+  ANDAMENTO = 'ANDAMENTO',
+  ATRASO = 'ATRASO',
+  CONCLUIDO = 'CONCLUIDO',
+  PENDENTE = 'PENDENTE'
+}
--- src/app/pages/tasks/tasklist/tasklist.component.ts
+++ src/app/pages/tasks/tasklist/tasklist.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
 @Component({
-  selector: 'app-tasklist',
+  selector: 'app-tasklist, MdbRippleModule',
   imports: [RouterLink],
   templateUrl: './tasklist.component.html',
   styleUrl: './tasklist.component.scss'
